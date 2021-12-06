be0d7b292a358d5a8366ad74980e939e6675153e
Todd Patton <64681278+Todd-ops@users.noreply.github.com>
2021-12-06 13:02:40 -0600

    Added navigation between 2 components

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
@@ -7,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   collapsed = true;
+
+  @Output() featureSelected = new EventEmitter<string>();
+
+  onSelect(feature: string){
+    this.featureSelected.emit(feature)
+  }
+
   constructor() { }
 
   ngOnInit(): void {
